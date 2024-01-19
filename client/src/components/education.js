431e1d2118e3f6b90ab4d8a06b12884a888818c6
Sulima0 <muhammadsulieman060@gmail.com>
2024-01-19 18:39:40 +0500

    Changes Maked

--- client/src/components/education.js
+++ client/src/components/education.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./StickyNavbar";
 import "../App.css";
 
 
 
 const Education = () => {
+
+   const[education, setEducation] = useState({
+    schoolName : '',
+    city : '',
+    state : '',
+    degree : '',
+    fieldOfStudy : '',
+    graduationDate : ''
+    
+   })
+
+   const handlechange = (e) => {
+    e.persist();
+    setEducation({...education, [e.target.name] : e.target.value});
+   }
+
+   const saveEducationDetail = (e) =>{
+    e.preventDefault();
+    const data ={
+      schoolName : education.schoolName,
+      city : education.schoolName,
+      state : education.schoolName,
+      degree : education.schoolName,
+      fieldOfStudy : education.schoolName,
+      graduationDate : education.schoolName
+   }
+  }
+
+
   return (
     <div className="forms">
       <Navbar />
@@ -15,27 +44,27 @@ const Education = () => {
             <p>EDUCATION</p>
             <span>Add information about your educational background.</span>
           </div>
-        <form>
+        <form onSubmit={saveEducationDetail}>
 
           <div className="inputboxes">
             <div className="left-side">
               <label for="schoolName">School Name:</label>
-              <input type="text" id="schoolName" name="schoolName" required placeholder="e.g., Your last school name"></input>
+              <input type="text" id="schoolName" name="schoolName" required placeholder="e.g., Your last school name" value={education.schoolName} onChange={handlechange}></input>
             </div>
             <div>
               <label for="city">City:</label>
-              <input type="text" id="city" name="city" required placeholder="e.g., school city name"></input>
+              <input type="text" id="city" name="city" required placeholder="e.g., school city name" value={education.city} onChange={handlechange}></input>
             </div>
           </div>
 
           <div className="inputboxes">
             <div className="left-side">
               <label for="state">State:</label>
-              <input type="text" id="state" name="state" required placeholder="e.g., sate name of your school"></input>
+              <input type="text" id="state" name="state" required placeholder="e.g., sate name of your school" value={education.state} onChange={handlechange}></input>
             </div>
             <div>
               <label for="degree">Select a Degree:</label>
-              <select id="degree" name="degree" required>
+              <select id="degree" name="degree" required value={education.degree} onChange={handlechange}>
                 <option value="bachelor">Bachelor's Degree</option>
                 <option value="master">Master's Degree</option>
                 <option value="doctorate">Doctorate</option>
@@ -46,11 +75,11 @@ const Education = () => {
           <div className="inputboxes">
             <div className="left-side">
               <label for="fieldOfStudy">Field of Study:</label>
-              <input type="text" id="fieldOfStudy" name="fieldOfStudy" required placeholder="e.g., Computer Science"></input>
+              <input type="text" id="fieldOfStudy" name="fieldOfStudy" required placeholder="e.g., Computer Science" value={education.fieldOfStudy} onChange={handlechange}></input>
             </div>
             <div>
               <label for="graduationDate">Graduation Date:</label>
-              <input type="date" id="graduationDate" name="graduationDate" required></input>
+              <input type="date" id="graduationDate" name="graduationDate" value={education.graduationDate} onChange={handlechange} required></input>
             </div>
           </div>
           <div className="exp-btns">
@@ -62,4 +91,6 @@ const Education = () => {
     </div>
   )
 };
+}
+
 export default Education;
--- client/src/components/experience.js
+++ client/src/components/experience.js
@@ -4,13 +4,39 @@ import Navbar from "./StickyNavbar";
 
 
 const Experience = () => {
+  const[experience, setExperience] = useState({
+    employment : '',
+    jobTitle : '',
+    city : '',
+    state : '',
+    startDate : '',
+    endDate : ''
+    
+   })
+
+   const handlechange = (e) => {
+    e.persist();
+    setExperience({...experience, [e.target.name] : e.target.value});
+   }
+
+   const saveEducationDetail = (e) =>{
+    e.preventDefault();
+    const data ={
+      employment : experience.employment,
+      jobTitle : experience.jobTitle,
+      city : experience.city,
+      state : experience.state,
+      startDate : experience.startDate,
+      endDate : experience.endDate
+   }
+  }
   return (
     <div className="forms">
       <Navbar />
 
 
       <div class="container">
-        <form>
+        <form on onSubmit={saveEducationDetail}>
           <div className="head">
             <div className="heading">
               <p>EXPERIENCE</p>
@@ -20,37 +46,37 @@ const Experience = () => {
           <div class="inputboxes">
             <div class="left-side">
               <label for="employment" class="Label">Employment Status</label>
-              <input type="text" id="employment" name="employment" placeholder="e.g., Full-time, Part-time, Self-employed"></input>
+              <input type="text" id="employment" name="employment" placeholder="e.g., Full-time, Part-time, Self-employed" value={experience.employment} onChange={handlechange}></input>
 
             </div>
 
             <div class="input-group">
               <label for="jobTitle" class="Label">Job Title</label>
-              <input type="text" id="jobTitle" name="jobTitle" placeholder="e.g., Web Developer"></input>
+              <input type="text" id="jobTitle" name="jobTitle" placeholder="e.g., Web Developer" value={experience.jobTitle} onChange={handlechange}></input>
             </div>
           </div>
 
           <div class="inputboxes">
             <div class="left-side">
               <label for="city" class="Label">City</label>
-              <input type="text" id="city" name="city" placeholder="e.g., Islamabad"></input>
+              <input type="text" id="city" name="city" placeholder="e.g., Islamabad" value={experience.city} onChange={handlechange}></input>
             </div>
 
             <div>
               <label for="state" class="Label">State</label>
-              <input type="text" id="state" name="state" placeholder="e.g., Panjab"></input>
+              <input type="text" id="state" name="state" placeholder="e.g., Panjab" value={experience.state} onChange={handlechange}></input>
             </div>
           </div>
 
           <div class="inputboxes">
             <div class="left-side">
               <label for="startDate" class="Label">Start Date</label>
-              <input type="date" id="startDate" name="startDate" ></input>
+              <input type="date" id="startDate" name="startDate" value={experience.startDate} onChange={handlechange}></input>
             </div>
 
             <div>
               <label for="endDate" class="Label">End Date</label>
-              <input type="date" id="endDate" name="endDate"></input>
+              <input type="date" id="endDate" name="endDate" value={experience.endDate} onChange={handlechange}></input>
             </div>
           </div>
 
@@ -66,5 +92,6 @@ const Experience = () => {
 
   )
 };
+
 
 export default Experience;
